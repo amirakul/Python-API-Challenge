90f2e1298c4bf81c235bd728222c296336a9bd69
Jildiz <zmurzaeva@gmail.com>
2020-08-02 23:35:51 -0500

    changed API keys

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "eb1a4356ca606ab51709e730af7b4ae9"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDHlhvPxBW5lQmpcGEuTpkHBynCQR8lTVQ"
+g_key = ""
